fed4b0f4b1d1bcde5951bff3736e3b432df709be
Frank Castro <95668208+frankproductdesign@users.noreply.github.com>
2022-05-01 22:49:46 -0400

    routing testing working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
@@ -7,6 +8,9 @@ import './App.css';
 
 const App = () => (
   <div className="App">
+    <Route path="/footer">
+      <Footer />
+    </Route>
     <div className="gradient__bg">
       <Navbar />
       <Header />
